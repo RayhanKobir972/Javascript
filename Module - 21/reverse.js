f70e28cb78d4bd76150e81f8dd8db00b6f88f9c7
Rayhan Kobir <rayhankobir972@gmail.com>
2023-08-05 20:07:59 +0600

    added module 22 problem in js

--- Module - 21/reverse.js
+++ Module - 21/reverse.js
@@ -1,9 +1,27 @@
 function reverseString(text){
+    let reversed ='';
     for(let i = text.length-1; i >= 0; i--){
         const element = text[i];
-        console.log(element);
+        reversed = reversed + element;
+        //console.log(element,reversed);
     }
+    return reversed;
 }
+
+
+function reverseWords(str){
+    const words = str.split(' ');
+    const result = [];
+    for(let i = words.length-1; i >= 0; i--){
+        result.push(words[i]);
+    }
+    const reversed = result.join(' ');
+    return reversed;
+}
+
 
 const myString ='I am a good boy';
 const reversed = reverseString(myString);
+const myString1 ='I am a good boy';
+const reversed1 = reverseWords(myString1);
+console.log(reversed1);
